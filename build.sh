1edfbc46aacde7877ade70dd782c65b61662ecb5
tenn0 <rougerom9@gmail.com>
2022-03-08 16:48:42 +0100

    add build ready notification for fun

--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@ pip3 uninstall -y TheengsGateway
 python3 setup.py sdist
 cd dist
 pip3 install TheengsGateway-0.1.4.tar.gz --no-deps
+curl --data "compiling durch, bruder" http://192.168.2.64:12101/api/text-to-speech
