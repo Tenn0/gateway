07260c9dbf18b9d685086ce3c20ba1c72f73431e
tenn0 <rougerom9@gmail.com>
2022-03-10 22:00:09 +0100

    1343

--- test/test.py
+++ test/test.py
@@ -41,7 +41,7 @@ def returnValues(device):
       properties = json.loads(properties)
       print(f"lol {properties}")
       #properties = properties['properties']
-      print(f"properties: {properties.keys}")
+      print(f"properties: {properties.keys()}")
 
       print(getAttribute(device, properties["steps"]))
       for p in properties.keys():
