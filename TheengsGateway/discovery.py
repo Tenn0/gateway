107fd68d0c92c9a63ae1b6bbfa3369799f3a9b70
tenn0 <rougerom9@gmail.com>
2022-03-12 21:28:39 +0100

    memory optimizing

--- TheengsGateway/discovery.py
+++ TheengsGateway/discovery.py
@@ -44,36 +44,34 @@ class discovery(gateway):
         topic = self.discovery_topic + "/" + pub_device_uuid + "/"
 
         #setup entities:
-        for p in pub_device['properties']:
+        #for p in pub_device['properties']:
           
-          print(f"p: {p}")
-          print(pub_device['properties'])
-          data = getProperties(pub_device['model_id'])
-          data = json.loads(data)
-          data = data['properties']  ##attributes  
-          for k in data.keys():
-                  print(data)
-                  print(f"k: {k}, type: {type(k)}")
-                  device['unique_id'] = pub_device['id'] + k
-
-                  device['name'] = hadevice['name'] + k
-                  state_topic = topic + k +"/state"
-                  config_topic = topic + k + "/config"
-                  attr_topic = topic + k + "/attributes"#k = json.loads(k)                    
-                  device['device'] = ha
-                  device['schema'] = "json"
-                  device['state_topic'] = state_topic
-                                  
-                  attributes = {}      
-                  attributes['rssi'] = pub_device['rssi']
-                  attributes['brand'] = pub_device['brand']
-                  attributes['id'] = pub_device['id']
-                  attributes['model'] = pub_device['model']
-                  attributes['model_id'] = pub_device['model_id']
-                  device['state_topic'] = state_topic
-                  attributes = json.dumps(attributes)
-                  device['json_attr_t'] = attr_topic
-                  if k in pub_device:
+         # print(f"p: {p}")
+        print(pub_device['properties'])
+        data = getProperties(pub_device['model_id'])
+        data = json.loads(data)
+        data = data['properties']  ##attributes  
+        for k in data.keys():
+           print(data)
+           print(f"k: {k}, type: {type(k)}")
+           device['unique_id'] = pub_device['id'] + k
+           device['name'] = hadevice['name'] + k
+           state_topic = topic + k +"/state"
+           config_topic = topic + k + "/config"
+           attr_topic = topic + k + "/attributes"#k = json.loads(k)                    
+           device['device'] = ha
+           device['schema'] = "json"
+           device['state_topic'] = state_topic                     
+           attributes = {}      
+           attributes['rssi'] = pub_device['rssi']
+           attributes['brand'] = pub_device['brand']
+           attributes['id'] = pub_device['id']
+           attributes['model'] = pub_device['model']
+           attributes['model_id'] = pub_device['model_id']
+           device['state_topic'] = state_topic
+           attributes = json.dumps(attributes)
+           device['json_attr_t'] = attr_topic
+           if k in pub_device:
                   #if k['name']:
                     print(f"property: {k}: {pub_device[k]} {k}")
                   #attributes['unit_of_meas'] = pub_device['attributes']
