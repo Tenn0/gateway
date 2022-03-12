a8ed8455d91a5d622ce6897d705c6687e4812fc8
tenn0 <rougerom9@gmail.com>
2022-03-12 18:33:08 +0100

    3123

--- TheengsGateway/discovery.py
+++ TheengsGateway/discovery.py
@@ -78,7 +78,7 @@ class discovery(gateway):
                     print(f"property: {k}: {pub_device[k]} {k}")
                   #attributes['unit_of_meas'] = pub_device['attributes']
                     msg = pub_device[k]
-                    print(msg)
+                    print(state_topic)
                     self.publish(msg, state_topic) 
                     self.publish(attributes, attr_topic) ##attributes
       
