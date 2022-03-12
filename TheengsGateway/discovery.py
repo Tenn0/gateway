9ef59144914632bf164bb8bc5a1cebc00b39f911
tenn0 <rougerom9@gmail.com>
2022-03-12 21:01:53 +0100

    jkhk

--- TheengsGateway/discovery.py
+++ TheengsGateway/discovery.py
@@ -56,7 +56,7 @@ class discovery(gateway):
                   print(f"k: {k}, type: {type(k)}")
                   device['unique_id'] = pub_device['id'] + p
 
-                  device['name'] = hadevice['name'] + p
+                  device['name'] = hadevice['name'] + k
                   state_topic = topic + k +"/state"
                   config_topic = topic + k + "/config"
                   attr_topic = topic + k + "/attributes"#k = json.loads(k)                    
