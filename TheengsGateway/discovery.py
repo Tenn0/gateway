bf64feb77128cbc21b7ac9f4af41af2ced582cdd
tenn0 <rougerom9@gmail.com>
2022-03-12 16:55:10 +0100

    3213

--- TheengsGateway/discovery.py
+++ TheengsGateway/discovery.py
@@ -37,32 +37,35 @@ class discovery(gateway):
         pub_device_uuid = pub_device_uuid.replace(':', '')
         device['unique_id'] = pub_device['id']
         topic = self.discovery_topic + "/" + pub_device_uuid
-        state_topic = topic + "/state"
-        config_topic = topic + "/config"
-        attr_topic = topic + "/attributes"
-        if 'name' in pub_device:
-          device['name'] = pub_device['name']
-        else: 
+
+        #setup entities:
+        for p in pub_device['properties']:
+          state_topic = topic + "/state"
+          config_topic = topic + "/config"
+          attr_topic = topic + "/attributes"
+          if 'name' in pub_device:
+            device['name'] = pub_device['name']
+          else: 
             device['name'] = pub_device_uuid
-        device['state_topic'] = state_topic
-        device['device'] = ha
-        device['schema'] = "json"
-        device['state_topic'] = state_topic
-        data = getProperties(pub_device['model_id'])
-        data = json.loads(data)
-        data = data['properties']  ##attributes
-        attributes = {}      
-        attributes['rssi'] = pub_device['rssi']
-        attributes['brand'] = pub_device['brand']
-        attributes['id'] = pub_device['id']
-        attributes['model'] = pub_device['model']
-        attributes['model_id'] = pub_device['model_id']
+          device['state_topic'] = state_topic
+          device['device'] = ha
+          device['schema'] = "json"
+          device['state_topic'] = state_topic
+          data = getProperties(pub_device['model_id'])
+          data = json.loads(data)
+          data = data['properties']  ##attributes
+          attributes = {}      
+          attributes['rssi'] = pub_device['rssi']
+          attributes['brand'] = pub_device['brand']
+          attributes['id'] = pub_device['id']
+          attributes['model'] = pub_device['model']
+          attributes['model_id'] = pub_device['model_id']
         
-        attributes = json.dumps(attributes)
-        device['json_attr_t'] = attr_topic
-        self.publish(attributes, attr_topic) ##attributes
-        print(data.keys())
-        for k in data.keys():
+          attributes = json.dumps(attributes)
+          device['json_attr_t'] = attr_topic
+          self.publish(attributes, attr_topic) ##attributes
+          print(data.keys())
+          for k in data.keys():
                   print(data)
                   print(f"k: {k}, type: {type(k)}")
                   #k = json.loads(k)
@@ -76,6 +79,6 @@ class discovery(gateway):
       
         
         
-        payload = json.dumps(device)
-        msg = payload
-        self.publish(msg, config_topic) ##overall device
+          payload = json.dumps(device)
+          msg = payload
+          self.publish(msg, config_topic) ##overall device
