5a5b9aea4b1e5cdfa4213e5ad4b6c4ab3b4b0b11
tenn0 <rougerom9@gmail.com>
2022-03-12 17:11:48 +0100

    3123

--- TheengsGateway/discovery.py
+++ TheengsGateway/discovery.py
@@ -22,6 +22,7 @@ class discovery(gateway):
     def publish_device_info(self, pub_device):  ##publish sensor directly to home assistant via mqtt discovery
         print(f"publishing device `{pub_device}`")
         pub_device['properties'] = json.loads(getProperties(pub_device['model_id']))
+        pub_device['properties'] = pub_device['properties']['properties']
         print(type(pub_device))
         print(pub_device['properties'].keys())
 
